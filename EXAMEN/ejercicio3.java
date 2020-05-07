36344c31bbe85f03775b812916fba3e270a8acf1
Rubenchuquivilca <38230003+Rubenchuquivilca@users.noreply.github.com>
2020-05-07 15:05:20 -0500

    ejercicio3

--- EXAMEN/ejercicio3.java
+++ EXAMEN/ejercicio3.java
@@ -1,5 +1,17 @@
 package EXAMEN;
 
+
 public class ejercicio3 {
-    
+
+    public static void main(String[] args) {
+        System.out.println(factorial(3));
+    }
+
+    public static int factorial(int n) {
+        int resultado = 1;
+        for (int i = 1; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
 }
